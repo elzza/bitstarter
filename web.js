b02c9f7802a3c9eb086e9e880bc98f0accc7ed38
Elzbieta Zawislak <elisa.zawislak@gmail.com>
2013-07-11 07:13:08 +0000

    reading from index

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var resp= fs.readFileSync("index.html").toString();
+  response.send(resp);
 });
 
 var port = process.env.PORT || 5000;
